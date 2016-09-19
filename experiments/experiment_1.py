609a332e0a9ed1d70602764028cf99b3f176dc9c
akshay bhat <akshayubhat@gmail.com>
2016-09-19 17:29:40 -0400

    improved OS guide

--- experiments/experiment_1.py
+++ experiments/experiment_1.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
-import time
+import time,os
 
 
 if __name__ == '__main__':
+    some_file = open("{}.txt".format(os.getpid()),'w')
     some_data = []
     for k in range(60):
         some_data += [1,] * 10000
         time.sleep(30)
+        some_file.write("{}\n".format(k))
+    some_file.close()
